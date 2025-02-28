9edc827aaa13c443cf310730d94b521f37e9cada
AnasKhan44 <anaskhan.ak2005@gmail.com>
2025-02-28 23:56:20 +0500

    Fixes

--- growth-mindset.py
+++ growth-mindset.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import os
 from io import BytesIO
-import openpyxl  # Ensure this is imported
+# Ensure this is imported
 
 st.set_page_config(page_title="Data Sweeper", layout="wide")
 st.title("Data Sweeper")
